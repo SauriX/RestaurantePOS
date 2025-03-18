4765a6d88daa7e17d2426266af56d6b9bf6f631a
SauriX <sauri618@gmail.com>
2025-03-17 20:47:14 -0500

    seeddata

--- RestaurantePOS/Domain/Users/UserType.cs
+++ RestaurantePOS/Domain/Users/UserType.cs
@@ -2,6 +2,17 @@
 {
     public class UserType
     {
+
+
+        public UserType(int Id_UserType, string NameType)
+        {
+            this.Id_UserType = Id_UserType;
+            this.NameType = NameType;
+        }
+        public UserType()
+        {
+    
+        }
         public int Id_UserType { get; set; }
         public string NameType { get; set; }
 
---
+++ RestaurantePOS/context/SeedData/SeedDataUserType.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantePOS.Domain.Users;
+
+namespace RestaurantePOS.context.SeedData
+{
+    public class SeedDataUserType
+    {
+        // Tipos de usuario
+        public static List<UserType> GetUserType()
+        {
+            var UserType = new List<UserType>()
+            {
+                new UserType(1, "Administrador"),
+                new UserType(2, "Vendedor"),
+                new UserType(3, "REPARTIDOR" ),
+
+            };
+
+            return UserType;
+        }
+    }
+}
